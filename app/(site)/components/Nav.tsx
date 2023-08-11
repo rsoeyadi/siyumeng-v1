05be952d6b54d3c750cfe108f4abf55b7d5d060d
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-10 23:44:59 -0400

    moved to right side

--- app/(site)/components/Nav.tsx
+++ app/(site)/components/Nav.tsx
@@ -8,8 +8,8 @@ export default function Nav() {
   const Sidebar = () => {
     return (
       <div
-        className={`top-0 left-0 w-[90vw] bg-pink-800 p-10 pl-20 text-white fixed h-full z-40 transition ease-in-out duration-300 transform ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
+        className={`top-0 right-0 w-[90vw] bg-pink-800 p-10 pl-20 text-white fixed h-full z-40 transform transition-transform ${
+          isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <h2 className="mt-20 text-4xl font-semibold text-white">
@@ -21,7 +21,7 @@ export default function Nav() {
 
   return (
     <div className="md:hidden">
-      <div className="absolute top-3 left-3 z-50">
+      <div className="absolute top-3 right-3 z-50">
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
       <Sidebar />
