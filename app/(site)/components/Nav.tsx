011a8de3db487d349d8574b95d03f4a3ac5a0539
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-10 22:06:01 -0400

    positioned hamburger menu

--- app/(site)/components/Nav.tsx
+++ app/(site)/components/Nav.tsx
@@ -1,9 +1,13 @@
-"use client"
+"use client";
 import { Spiral as Hamburger } from "hamburger-react";
 import { useState } from "react";
 
 export default function Nav() {
   const [isOpen, setOpen] = useState(false);
 
-  return <Hamburger toggled={isOpen} toggle={setOpen} />;
+  return (
+    <div className="lg:hidden absolute top-3 left-3">
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+    </div>
+  );
 }
