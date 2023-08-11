2e4b48487537510be88a49009b1219add359da60
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-11 16:44:58 -0400

    added sidebar animation. Had to remove the sidebar component and use it directly

--- app/(site)/components/Nav.tsx
+++ app/(site)/components/Nav.tsx
@@ -5,26 +5,20 @@ import { useState } from "react";
 export default function Nav() {
   const [isOpen, setOpen] = useState(false);
 
-  const Sidebar = () => {
-    return (
-      <div
-        className={`top-0 right-0 w-[90vw] bg-pink-800 p-10 pl-20 text-white fixed h-full z-40 transform transition-transform ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <h2 className="mt-20 text-4xl font-semibold text-white">
-          I am a sidebar #0F171D
-        </h2>
-      </div>
-    );
-  };
-
   return (
-    <div className="md:hidden">
+    <>
       <div className="absolute top-3 right-3 z-50">
         <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
-      <Sidebar />
-    </div>
+      <div
+        className={`top-0 right-0 w-[90vw] bg-pink-800 p-10 pl-20 text-white fixed h-full z-40 ease-in-out duration-300 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <h3 className="mt-20 text-4xl font-semibold text-white">
+          I am a sidebar
+        </h3>
+      </div>
+    </>
   );
 }
