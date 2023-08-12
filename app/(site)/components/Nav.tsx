0b711a3839b5968fe726e47b6e321457ef369766
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-11 20:56:49 -0400

    fixed overflow issue

--- app/(site)/components/Nav.tsx
+++ app/(site)/components/Nav.tsx
@@ -1,23 +1,46 @@
 "use client";
 import { Spiral as Hamburger } from "hamburger-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Nav() {
   const [isOpen, setOpen] = useState(false);
+  useEffect(() => {
+    setOpen(isOpen);
+    if (isOpen) {
+      document.body.classList.add("overflow-y-hidden");
+    } else {
+      document.body.classList.remove("overflow-y-hidden");
+    }
+  }, [isOpen]);
+  const pages = [
+    { title: "Home", route: "/" },
+    { title: "Biography", route: "/biography" },
+    { title: "Events", route: "/events" },
+    { title: "Videos", route: "/videos" },
+    { title: "Gallery", route: "/gallery" },
+    { title: "Teaching", route: "/teaching" },
+    { title: "Contact", route: "/contact" },
+  ];
 
   return (
     <>
-      <div className="absolute top-3 right-3 z-50">
-        <Hamburger toggled={isOpen} toggle={setOpen} />
+      <div className="block bg-slate-100 w-screen top-0 left-0 h-20">
+        <div className="absolute top-3 right-3 z-50">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </div>
       </div>
       <div
-        className={`top-0 right-0 w-[90vw] bg-pink-800 p-10 pl-20 text-white fixed h-full z-40 ease-in-out duration-300 ${
+        className={`top-20 right-0 w-[100vw] bg-slate-100 p-10 pl-20 text-white fixed h-full z-40 ease-in-out duration-300 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <h3 className="mt-20 text-4xl font-semibold text-white">
-          I am a sidebar
-        </h3>
+        <ul className="text-right">
+          {pages.map((page) => (
+            <li key={page.route} className="pb-5">
+              <p className="text-xl">{page.title}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
