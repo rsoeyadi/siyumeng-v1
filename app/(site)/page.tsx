da24e48138407912502707b0020e93fd40d9335e
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-11 23:31:58 -0400

    looks like Netflix landing page now

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,15 +17,34 @@ export default function Home() {
     backgroundPosition: "center top", // Adjust this line
     backgroundRepeat: "no-repeat",
     width: "100vw",
-    height: "95vh",
+    height: "115vh",
+    display: "flex", // Add this line
+    alignItems: "flex-end", // Add this line
+    justifyContent: "center", // Add this line
   };
 
   return (
-    <div>
-      <div style={backgroundImageStyle}></div>
-      <div className="before:-top-24 landscape-sm:before:-top-9 before:absolute before:left-0 before:right-0 before:h-56 landscape-sm:before:h-36 before:bg-white -skew-y-6 sm:-top-12"></div>
-
-      <h1>BIOGRAPHY</h1>
+    <div className="relative">
+      <div className="uppercase" style={backgroundImageStyle}>
+        <div
+          className="text-white text-center"
+          style={{ marginBottom: "30vh", position: "relative", zIndex: 1 }}
+        >
+          <p className="text-4xl text-white font-bold">
+            Siyumeng Wang, pianist
+          </p>
+          <p className="text-3xl text-white font-bold pt-10">Biography</p>
+          <div className="border-b-2 border-white mx-auto w-10 mt-2"></div>
+        </div>
+      </div>
+      <div
+        className="absolute inset-0 bg-black opacity-30 z-0"
+        style={{ mixBlendMode: "multiply" }}
+      ></div>
+      <div
+        className="-z-20 before:-top-32 before:absolute before:left-0 before:right-0 before:h-64 before:bg-white -skew-y-6 sm:-top-12"
+        style={{ zIndex: -1 }}
+      ></div>
     </div>
   );
 }
