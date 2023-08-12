1ff11d121bdd30a890f56b20cff4adf979d09cca
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-12 10:35:01 -0400

    positioning

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -24,24 +24,41 @@ export default function Home() {
 
   return (
     <div className="relative">
-      <div className="h-[80vh] md:h-[105vh]" style={backgroundImageStyle}>
+      <div
+        className="h-[80vh] landscape-sm:h-[125vh] md:h-[105vh] background-image"
+        style={{
+          backgroundImage: `url('${data.image}')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center top",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          display: "flex",
+          alignItems: "flex-end",
+          justifyContent: "center",
+        }}
+      >
+
         <div
-          className="text-white text-center"
-          style={{ marginBottom: "33vh", position: "relative", zIndex: 1 }}
+          className="text-white text-center mb-[22vh] landscape-sm:mb-[32vh] md:mb-[30vh] text-content"
+          style={{ position: "relative", zIndex: 1 }}
         >
-          <p className="text-4xl text-white font-bold uppercase">
+          <p className="text-4xl px-2 text-white font-bold uppercase name">
             Siyumeng Wang, pianist
           </p>
-          <p className="text-3xl text-white font-bold pt-10">Biography</p>
-          <div className="border-b-2 border-white mx-auto w-10 mt-2 landscape-sm:mb-5"></div>
+          <p className="text-3xl text-white font-bold pt-6 pb-2 bio">
+            Biography
+          </p>
+          <div className="border-b-2 border-white mx-auto w-10 divider landscape-sm:mb-5"></div>
         </div>
       </div>
+
       <div
-        className="absolute inset-0 bg-black opacity-30 z-0"
+        className="absolute inset-0 bg-black opacity-30 z-0 overlay"
         style={{ mixBlendMode: "multiply" }}
       ></div>
+
       <div
-        className="-z-20 before:-top-32 before:absolute before:left-0 before:right-0 before:h-64 before:bg-white -skew-y-6 sm:-top-12"
+        className="-z-20 before:-top-32 before:absolute before:left-0 before:right-0 before:h-64 before:bg-white -skew-y-6 sm:-top-12 shapes"
         style={{ zIndex: -1 }}
       ></div>
     </div>
