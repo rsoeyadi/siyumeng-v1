e971d966a6a89d4fa4e330aac76e7f6d092f972b
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-13 16:12:24 -0400

    three columns on desktop

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -2,21 +2,18 @@ import SocialLinks from "./SocialLinks";
 
 export default function Footer() {
   return (
-    <div className="grid grid-rows-3 gap-4 bg-slate-700 py-7 mt-28">
-      <div className="text-center flex items-center justify-center">
-        <span className="text-white text-3xl uppercase ">
-          Siyumeng Wang
-        </span>
+    <div className="grid lg:grid-cols-3 gap-4 bg-slate-700 py-7 mt-28">
+      <div className="lg:col-span-1 text-center lg:flex lg:items-center lg:justify-center">
+        <span className="text-white text-3xl lg:text-4xl uppercase">Siyumeng Wang</span>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="lg:col-span-1 flex items-center justify-center">
         <SocialLinks />
       </div>
-
-      <div className="text-white">
-        <div className="text-white text-center">
+      <div className="lg:col-span-1 text-center">
+        <div className="text-white">
           &copy; {new Date().getFullYear()} Siyumeng Wang.
         </div>
-        <div className="text-white text-center">ALL RIGHTS RESERVED</div>
+        <div className="text-white">ALL RIGHTS RESERVED</div>
       </div>
     </div>
   );
