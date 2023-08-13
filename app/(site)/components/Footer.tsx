64f07e9d99085509cfd622f3456d9d023083790c
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-13 14:31:24 -0400

    starting footer

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <><div>HELLO</div></>;
+  return (
+    <div className="w-full max-h-48 bg-slate-700 grid grid-rows-4">
+      <p className="text-white">Siyumeng Wang</p>
+      <div className="grid grid-cols-3"></div>
+      <div>© {new Date().getFullYear()} Siyumeng Wang.</div>
+      <div> ALL RIGHTS RESERVED</div>
+    </div>
+  );
 }
