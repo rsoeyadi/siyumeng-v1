5fb117f1b0f9dee4d8cf0611cdf5a0f9e1c095eb
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-13 16:21:23 -0400

    .

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -2,9 +2,11 @@ import SocialLinks from "./SocialLinks";
 
 export default function Footer() {
   return (
-    <div className="grid lg:grid-cols-3 gap-4 bg-slate-700 py-7 mt-28">
+    <div className=" grid lg:grid-cols-3 gap-4 bg-slate-700 py-7 mt-28">
       <div className="lg:col-span-1 text-center lg:flex lg:items-center lg:justify-center">
-        <span className="text-white text-3xl lg:text-4xl uppercase">Siyumeng Wang</span>
+        <span className="z-[10000] text-white text-3xl lg:text-4xl uppercase">
+          Siyumeng Wang
+        </span>
       </div>
       <div className="lg:col-span-1 flex items-center justify-center">
         <SocialLinks />
