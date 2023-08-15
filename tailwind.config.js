74fd837f58c3654a3a787dd236b43777b7f46b7c
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-15 14:03:51 -0400

    I removed some comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       screens: {
-        // Common mobile sizes
         sm: "640px", // Small screens
         md: "768px", // Medium screens
         lg: "1024px", // Large screens
