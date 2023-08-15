7cbe736571c02aa5861f44b02945a71759a57050
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-14 22:36:27 -0400

    hover

--- app/(site)/components/Event.tsx
+++ app/(site)/components/Event.tsx
@@ -37,8 +37,8 @@ export function Event({ sortedEvent }: eventProps) {
           <PortableText value={sortedEvent.description}></PortableText>
         </div>
         {sortedEvent.link && (
-          <p className="lg:text-sm lg:col-span-2 lg:col-start-11 uppercase pt-5 lg:pt-0 font-bold underline underline-offset-8	">
-            <Link href={sortedEvent.link} target="_blank">
+          <p className="lg:text-sm lg:col-span-2 lg:col-start-11  uppercase pt-5 lg:pt-0 font-bold underline underline-offset-8	">
+            <Link className="hover:text-slate-500 duration-300 ease-in-out" href={sortedEvent.link} target="_blank">
               More Information
             </Link>
           </p>
