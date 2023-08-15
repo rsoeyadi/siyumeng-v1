12a498123331fcf26c4416e957d224bb3c9c0278
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-15 13:56:17 -0400

    Waving hand

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -1,14 +1,23 @@
+import React from "react";
+
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="tilting-svg animate-waving-hand" // Apply a basic animation class
       >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M22 21C23.1046 21 24 20.1046 24 19V5C24 3.89543 23.1046 3 22 3H3C1.89543 3 1 3.89543 1 5V19C1 20.1046 1.89543 21 3 21H22ZM11 5H8.98486V13H7.98511V19H12V13H11V5ZM18.0151 19H22V5H19.0151V13H18.0151V19ZM17.0151 13H16.0151V5H14V13H13V19H17.0151V13ZM6.98511 19V13H5.98486V5H3L3 19H6.98511Z"
+          fill="currentColor"
+        />
+      </svg>
     </div>
   );
 }
