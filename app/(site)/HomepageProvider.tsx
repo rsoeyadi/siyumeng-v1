4f9b98db660ed6d38dd2fefefc8cfd07727e2bf2
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-15 20:55:13 -0400

    provider

---
+++ app/(site)/HomepageProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { getCoverPhotos } from "@/sanity/sanity-utils";
+import useSWR from "swr";
+import Loading from "./loading";
+
+export default function HomepageProvider() {
+  const { data, error, isLoading } = useSWR("coverPhotos", getCoverPhotos);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <Loading />;
+  const backgroundImageStyle = {
+    backgroundImage: `url('${data?.entryImage}')`,
+    backgroundSize: "cover",
+    backgroundPosition: "left 38% top 24%",
+    backgroundRepeat: "no-repeat",
+    width: "100vw",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  };
+  return (
+    <div className="relative">
+      <div
+        className="h-[100vh] background-image"
+        style={{
+          ...backgroundImageStyle,
+        }}
+      ></div>
+    </div>
+  );
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,30 +1,5 @@
-"use client";
-import { getCoverPhotos } from "@/sanity/sanity-utils";
-import useSWR from "swr";
-import Loading from "./loading";
+import HomepageProvider from "./HomepageProvider";
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR("coverPhotos", getCoverPhotos);
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <Loading />;
-  const backgroundImageStyle = {
-    backgroundImage: `url('${data?.entryImage}')`,
-    backgroundSize: "cover",
-    backgroundPosition: "left 38% top 24%",
-    backgroundRepeat: "no-repeat",
-    width: "100vw",
-    display: "flex",
-    alignItems: "flex-end",
-    justifyContent: "center",
-  };
-  return (
-    <div className="relative">
-      <div
-        className="h-[100vh] background-image"
-        style={{
-          ...backgroundImageStyle,
-        }}
-      ></div>
-    </div>
-  );
+  return <HomepageProvider />;
 }
