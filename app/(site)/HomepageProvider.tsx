4acf56d62838ce404830fcbd9011a86abc884476
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-09-09 10:56:41 -0400

    brightness upped

--- app/(site)/HomepageProvider.tsx
+++ app/(site)/HomepageProvider.tsx
@@ -5,7 +5,7 @@ export default function HomepageProvider() {
   return (
     <div className="relative">
       <div className={`h-[100vh]`}>
-        <CoverPhoto coverPhoto={cover} objectPosition="left 38% top 24%" />
+        <CoverPhoto coverPhoto={cover} objectPosition="left 38% top 24%" brightnessClass="brightness-30"/>
       </div>
     </div>
   );
