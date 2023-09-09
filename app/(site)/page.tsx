24c3db9398d96053312e6dd222a26d4d061ea0bf
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-09-09 00:59:59 -0400

    added meta data

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,15 @@
+import { Metadata } from "next";
 import HomepageProvider from "./HomepageProvider";
+
+export const metadata: Metadata = {
+  title: "Siyumeng Wang | Pianist",
+  description:
+    "Pianist Siyumeng Wang | Biography, events, videos, and more",
+  metadataBase: new URL("https://siyumeng.com"),
+  openGraph: {
+    images: "https://siyumeng.com/images/preview.jpg",
+  },
+};
 
 export default function Home() {
   return <HomepageProvider />;
